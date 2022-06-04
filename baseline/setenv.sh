048f9e791eb3ebabda758bb41df87d91ba4b082d
Adam V Stephen (ukaea-fpga) <adam.v.stephen@gmail.com>
2022-06-04 15:49:05 +0100

    Extend setenv.sh to create docker env file.

--- baseline/setenv.sh
+++ baseline/setenv.sh
@@ -5,16 +5,25 @@
 # 2022-01-22 passes shellcheck linting OK.
 # 2022-01-22 : bashtip : surround shell expansion strings in double quotes : shellcheck
 # 2022-01-22 : bashtip : use || exit after a cd attempt in case the directory is not found : shellcheck
+# 2022-06-04 : Add a function that generates a Docker env_file from the computed values
+# 
 # TODO: use the following to avoid any absolute path reference assumptions : install wherever cloned to.
 #set -x
+
 
 export SCRIPT="${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}"
 export SCRIPT_REALPATH=$(realpath "$SCRIPT")
 export SCRIPT_NAME=$(basename "$SCRIPT_REALPATH")
 export SCRIPT_DIR=$(dirname "$SCRIPT_REALPATH")
 
+if [ $# -eq 0 ]
+then
 export PATH=$PATH:${SCRIPT_DIR}
 export INSTALLATION_DIR=$(realpath "$SCRIPT_DIR/../../")
+else
+        INSTALLATION_DIR=$1
+        shift
+fi
 
 export MARTe2_PROJECT_ROOT=${INSTALLATION_DIR}/MARTe2-sigtools
 export MARTe2_Sigtools_Dependencies_Installed_File=${MARTe2_PROJECT_ROOT}/marte2-sigtools.deps.installed
@@ -45,3 +54,15 @@ export MARTe2_CONFIG_PATH=${MARTe2_Demos_Sigtools_DIR}/Configurations
 # Broadcast address of the specifc network
 export EPICS_CA_ADDR_LIST=10.208.19.255
 export EPICS_CA_AUTO_ADDR_LIST=NO
+
+make_docker_env(){
+        echo "Creating Docker ENV file env_docker"
+        > env_docker
+for var in MARTe2_DIR MARTe2_Components_DIR
+do
+        echo $var=${!var} | tee -a env_docker
+done
+}
+
+
+make_docker_env
